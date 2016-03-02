120f90aae66c2ca80f0643c5e936f01c57fe17b9
badhree <badhri.sadagopan@gmail.com>
2016-03-02 02:53:01 +0000

    Color

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ $(function() {
   for(var number=0; number<=100; number++)
   {
   	if((number%3==0)&& (number%5==0))
-  		$('#fizzbuzz').append('<p style="color:blue;">'+ 'FizzBuzz' +'</p>');
+  		$('#fizzbuzz').append('<p style=\"color:blue;\">'+ 'FizzBuzz' +'</p>');
   	else if(number%3==0)
-  		$('#fizzbuzz').append('<p>'+ 'Fizz' +'</p>');
+  		$('#fizzbuzz').append('<p style=\"color:red;\">'+ 'Fizz' +'</p>');
   	else if(number%5==0)
-  		$('#fizzbuzz').append('<p>'+ 'Buzz' +'</p>');
+  		$('#fizzbuzz').append('<p style=\"color:orange;\">'+ 'Buzz' +'</p>');
   	else 
-  		$('#fizzbuzz').append('<p>'+ number +'</p>');
+  		$('#fizzbuzz').append('<p style=\"color:green;\">'+ number +'</p>');
 	}
 }
 });
