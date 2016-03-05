e72f6f1608c2b916e2d29944b7f891f4c48dee7b
badhree <badhri.sadagopan@gmail.com>
2016-03-05 07:55:57 +0000

    Input

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-$(function() {
+$(document).ready(function(){
   
-  addFizzBuzz();
+function isNumber(n) {
+      if(!isNaN(parseInt(n)) && isFinite(n)) 
+        return true;
+      return false;
+    };
 
-  function addFizzBuzz() {
-  
-  for(var number=0; number<=100; number++)
+  /**/
+
+  function addFizzBuzz(n) {
+  $("#fizzbuzz").empty();
+  for(var number=0; number<=n; number++)
   {
-  	if((number%3==0)&& (number%5==0))
-  		$('#fizzbuzz').append('<p style=\"color:blue;\">'+ 'FizzBuzz' +'</p>');
-  	else if(number%3==0)
-  		$('#fizzbuzz').append('<p style=\"color:red;\">'+ 'Fizz' +'</p>');
-  	else if(number%5==0)
-  		$('#fizzbuzz').append('<p style=\"color:orange;\">'+ 'Buzz' +'</p>');
-  	else 
-  		$('#fizzbuzz').append('<p style=\"color:green;\">'+ number +'</p>');
-	}
-}
+    if((number%3==0)&& (number%5==0))
+      $('#fizzbuzz').append('<p style=\"color:blue;\">'+ 'FizzBuzz' +'</p>');
+    else if(number%3==0)
+      $('#fizzbuzz').append('<p style=\"color:red;\">'+ 'Fizz' +'</p>');
+    else if(number%5==0)
+      $('#fizzbuzz').append('<p style=\"color:orange;\">'+ 'Buzz' +'</p>');
+    else 
+      $('#fizzbuzz').append('<p style=\"color:green;\">'+ number +'</p>');
+    };
+  };
+
+
+$('#print').click(function()
+{
+  
+  var count = $('#count').val();
+  if(isNumber(count))
+    addFizzBuzz(count);
+  else
+    alert("Enter Integer");
+});;
+
+
 });
+
+
+
